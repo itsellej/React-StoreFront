2477f2ba2c73735ecb5550883173f5c84bd2abde
Elle Hallal <daniellehallal@gmail.com>
2019-03-07 08:40:01 +0000

    When user enters their details via /users/signup, a new user is created in users table of database

--- server/database/users.js
+++
@@ -1,8 +0,0 @@
-const login = (request, response) => {
-    response.status(200).json("login")
-}
-
-const signup = (request, response) => {
-    response.status(200).json("signup")
-}
-module.exports = { login, signup }
